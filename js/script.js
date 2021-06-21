0d3744b7b4bc400f6357c59261cb0660c32c7619
Hai Anh <67333654+phantomxxi@users.noreply.github.com>
2021-06-21 16:51:37 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let sair_menu = document.getElementById('sair-menu')
+let menu = document.getElementById('menu')
+let entrar_menu = document.getElementById('entrar-menu')
+
+function clicar_entrar_menu(){
+    menu.style.display='inline'
+}
+
+function clicar_sair_menu(){
+    menu.style.display='none'
+}
